a243c195f4525388f7813b50a7c091537f22086f
geoff.baldry1 <geoff.baldry1@ihsmarkit.com>
2023-02-01 10:49:19 +0000

    added serverless backend option

---
+++ backend/handler.js
@@ -0,0 +1,58 @@
+const serverless = require('serverless-http')
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const cloudManagerConnectorRoutes = require('./routes/cloudManagerConnectorRoutes')
+const aggregateRoutes = require('./routes/aggregateRoutes')
+const workingEnvironmentRoutes = require('./routes/workingEnvironmentRoutes')
+const volumeRoutes = require('./routes/volumeRoutes')
+const nodeRoutes = require('./routes/nodeRoutes')
+const instanceRoutes = require('./routes/instanceRoutes')
+const backupRoutes = require('./routes/backupRoutes')
+const providerVolumeRoutes = require('./routes/providerVolumeRoutes')
+const workingEnvironmentBackupRoutes = require('./routes/workingEnvironmentBackupRoutes')
+const authRoutes = require('./routes/authRoutes')
+const credentialRoutes = require('./routes/credentialRoutes')
+const DB = require('./services/db')
+
+const instance = DB.getInstance()
+
+// The above route 'requires' will cause the DB models to be created downstream,
+// and populate the sequelize.models variable. Use it to create the associations
+require('./models/netappcvo/associations')(instance.sequelize.models)
+
+// Now that the models and associations are created, call sync to push to the DB
+instance.sequelize.sync()
+
+const PORT = process.env.PORT
+
+// Create express app
+const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors({ origin: true, credentials: true }))
+
+// Routes must come after the other middlewares!
+app.use('/api/cloudManagerConnector', cloudManagerConnectorRoutes)
+app.use('/api/aggregate', aggregateRoutes)
+app.use('/api/workingEnvironment', workingEnvironmentRoutes)
+app.use('/api/volume', volumeRoutes)
+app.use('/api/providerVolume', providerVolumeRoutes)
+app.use('/api/node', nodeRoutes)
+app.use('/api/instance', instanceRoutes)
+app.use('/api/backup', backupRoutes)
+app.use('/api/workingEnvironmentBackup', workingEnvironmentBackupRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/credential', credentialRoutes)
+
+// ErrorHandler after routes
+app.use(errorHandler)
+
+// Not needed if using serverless
+// app.listen(PORT, () => {
+//   console.log('Server started on port ' + PORT)
+// })
+
+module.exports.handler = serverless(app)
--- backend/services/db.js
+++ backend/services/db.js
@@ -1,11 +1,22 @@
 const { Sequelize } = require('sequelize')
 
+const CURRENT_LAMBDA_FUNCTION_TIMEOUT = 100000 // ms
+
+const pool = {
+  max: 2, // Model.findAndCountAll issues 2 requests
+  min: 0, // Cause connection pool eviction to eventually clean all connections
+  idle: 0, // Make connections immediately available for cleanup after returning to pool
+  acquire: 3000, // Fail fast if a connection takes too long to be established
+  evict: CURRENT_LAMBDA_FUNCTION_TIMEOUT,
+}
+
 // A Singleton service class to contain calls to the Sequelize service
 class DB {
   constructor(dialect, storage) {
     this.sequelize = new Sequelize({
       dialect: dialect,
       storage: storage,
+      pool: pool,
     })
   }
 
